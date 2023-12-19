91eb582c0d921aa9603e3c6f320fa583fed2661e
ext.zhanglong5 <ext.zhanglong5@jd.com>
2023-12-19 16:56:25 +0800

    feat:添加express自动生成的项目

---
+++ 笔记/10 nodejs/node.js—资料/day8/code/api_server_new/app.js
@@ -0,0 +1,85 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/static', express.static(__dirname + '/public'));
+
+//todo从别的地方引入begin
+const joi = require('joi')
+// 导入并配置 cors 中间件
+const cors = require('cors')
+app.use(cors())
+// 托管静态资源文件
+app.use('/uploads', express.static('./uploads'))
+
+// 一定要在路由之前，封装 res.cc 函数
+app.use((req, res, next) => {
+  // status 默认值为 1，表示失败的情况
+  // err 的值，可能是一个错误对象，也可能是一个错误的描述字符串
+  res.cc = function (err, status = 1) {
+    res.send({
+      status,
+      message: err instanceof Error ? err.message : err,
+    })
+  }
+  next()
+})
+// 一定要在路由之前配置解析 Token 的中间件
+const expressJWT = require('express-jwt')
+const config = require('./config')
+
+app.use(expressJWT({ secret: config.jwtSecretKey }).unless({ path: [/^\/api/,'/'] }))
+// 导入并使用用户路由模块
+const userRouter = require('./router/user')
+app.get('/',(req,res)=>{
+  res.send({
+    code:'1',
+    msg:'你好吗,我很好!'
+  })
+})
+app.use('/api', userRouter)
+// 导入并使用用户信息的路由模块
+const userinfoRouter = require('./router/userinfo')
+app.use('/my', userinfoRouter)
+// 导入并使用文章分类的路由模块
+const artCateRouter = require('./router/artcate')
+app.use('/my/article', artCateRouter)
+// 导入并使用文章的路由模块
+const articleRouter = require('./router/article')
+app.use('/my/article', articleRouter)
+//todo从别的地方引入end
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // 验证失败导致的错误
+    if (err instanceof joi.ValidationError) return res.cc(err)
+    // 身份认证失败后的错误
+    if (err.name === 'UnauthorizedError') return res.cc('身份认证失败！')
+    // 未知的错误
+    res.cc(err)
+});
+
+module.exports = app;
